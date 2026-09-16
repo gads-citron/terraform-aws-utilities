cf2939e02a0f61ee6f8a45891b5fefd0f5539436
clement94310 <clement2323@hotmail.com>
2026-09-16 20:41:55 +0200

    fix version

--- examples/require-executable/main.tf
+++ examples/require-executable/main.tf
@@ -1,12 +1,5 @@
 terraform {
-<<<<<<< HEAD
   required_version = ">= 1.0.0,"
-=======
-  # Due to the HashiCorp BSL License change, we are restricting the latest version of Terraform to 1.5.7.
-  # Going forward we will be adding support for OpenTofu. For additional information on OpenTofu please 
-  # see the official OpenTofu website at https://opentofu.org/.
-  required_version = ">= 1.0.0"
->>>>>>> da789a5 (fix terraform version)
 }
 
 module "require_executables" {
