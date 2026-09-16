3014b5c405ef368d60688a707ad0aae2a95ddb15
clement94310 <clement2323@hotmail.com>
2026-09-16 17:19:09 +0200

    fix

--- examples/executable-dependency/main.tf
+++ examples/executable-dependency/main.tf
@@ -1,5 +1,8 @@
 terraform {
-  required_version = ">= 1.0.0,"
+  # Due to the HashiCorp BSL License change, we are restricting the latest version of Terraform to 1.5.7.
+  # Going forward we will be adding support for OpenTofu. For additional information on OpenTofu please 
+  # see the official OpenTofu website at https://opentofu.org/.
+  required_version = ">= 1.0.0"
 }
 
 module "executable" {
